28fdbb3f3c33f7b75b344933e36edc1b1e9281c1
Elvan H <elvanhd@gmail.com>
2022-03-15 20:47:31 +0700

    animation basics and css transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -15,12 +15,24 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      animatedBlock: false,
+      dialogIsVisible: false,
+    };
   },
+
   methods: {
+    animateBlock() {
+      this.animatedBlock = true;
+      setTimeout(() => {
+        this.animatedBlock = false;
+      }, 1000);
+    },
+
     showDialog() {
       this.dialogIsVisible = true;
     },
+
     hideDialog() {
       this.dialogIsVisible = false;
     },
@@ -32,12 +44,15 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: sans-serif;
 }
+
 body {
   margin: 0;
 }
+
 button {
   font: inherit;
   padding: 0.5rem 2rem;
@@ -47,17 +62,21 @@ button {
   color: white;
   cursor: pointer;
 }
+
 button:hover,
 button:active {
   background-color: #a80b48;
   border-color: #a80b48;
 }
+
 .block {
   width: 8rem;
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform 0.5s ease-out;
 }
+
 .container {
   max-width: 40rem;
   margin: 2rem auto;
@@ -68,5 +87,9 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  transform: translateX(-150px);
 }
 </style>

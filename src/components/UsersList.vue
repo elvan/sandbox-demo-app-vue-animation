fdc6c741b0823ae5d3bdff6aa3136ebeffd9f9f5
Elvan H <elvanhd@gmail.com>
2022-03-17 14:16:24 +0700

    creating list of users

---
+++ src/components/UsersList.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </ul>
+  <div>
+    <input type="text" ref="userNameInput" />
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ['Aaron', 'Bob', 'Cindy', 'Derek', 'Evan'],
+    };
+  },
+
+  methods: {
+    addUser() {
+      const enteredUserName = this.$refs.userNameInput.value;
+      this.users.unshift(enteredUserName);
+    },
+
+    removeUser(user) {
+      this.users = this.users.filter((u) => u !== user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+}
+li {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
